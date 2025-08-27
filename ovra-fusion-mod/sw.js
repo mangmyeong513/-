6d0ed2082a7bfaa1bf715b34170493374ee26a73
mangmyeong513 <mangmyeong513@gmail.com>
2025-08-28 08:38:14 +0900

    Delete ovra-fusion-mod directory

--- ovra-fusion-mod/sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('ovra-v1').then(c=>c.addAll(['/','/index.html','/styles.css','/src/main.js','/src/store.js','/src/users.js','/src/moderation.js','/src/components.js'])));
-});
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(res=> res || fetch(e.request)));
-});
